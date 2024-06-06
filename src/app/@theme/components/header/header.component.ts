55c61b2fb1fdb4b0de592b786ddea51692974d93
Francisco Alves Fernandes Junior <88057117+FranciscoJunior65@users.noreply.github.com>
2024-06-06 00:09:02 -0300

    versaodo mac

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -34,6 +34,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
       value: 'corporate',
       name: 'Corporate',
     },
+    {
+      value: 'material-dark',
+      name: 'Material Dark'
+    },
+    {
+      value: 'material-light',
+      name: 'Material Ligth'
+    }
   ];
 
   currentTheme = 'default';
