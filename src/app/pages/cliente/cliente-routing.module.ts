23fcb7b699842ff475cd786104f68d128ba99255
Francisco Alves Fernandes Junior <88057117+FranciscoJunior65@users.noreply.github.com>
2024-06-11 00:39:00 -0300

    Em desenvolvimento

--- src/app/pages/cliente/cliente-routing.module.ts
+++ src/app/pages/cliente/cliente-routing.module.ts
@@ -2,21 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ClienteComponent } from './cliente.component';
-import { TinyMCEComponent } from './tiny-mce/tiny-mce.component';
-import { CKEditorComponent } from './ckeditor/ckeditor.component';
 import { ClienteNovoComponent } from './cliente-novo/cliente-novo.component';
 
 const routes: Routes = [{
   path: '',
   component: ClienteComponent,
-  children: [{
-    path: 'tinymce',
-    component: TinyMCEComponent,
-    }, 
-    {
-      path: 'ckeditor',
-      component: CKEditorComponent,
-    },
+  children: [
     {
       path: 'cliente-novo',
       component: ClienteNovoComponent
@@ -32,7 +23,5 @@ export class ClienteRoutingModule { }
 
 export const routedComponents = [
   ClienteComponent,
-  TinyMCEComponent,
-  CKEditorComponent,
   ClienteNovoComponent
 ];
--- src/app/pages/cliente/cliente.module.ts
+++ src/app/pages/cliente/cliente.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
-import { NbCardModule } from '@nebular/theme';
+import { NbCardModule, NbInputModule } from '@nebular/theme';
 import { CKEditorModule } from 'ng2-ckeditor';
-
 import { ThemeModule } from '../../@theme/theme.module';
-
 import { ClienteRoutingModule, routedComponents } from './cliente-routing.module';
-import { ClienteNovoComponent } from './cliente-novo/cliente-novo.component';
+import { NbSelectModule } from '@nebular/theme';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
+    FormsModule,
     NbCardModule,
     ThemeModule,
     ClienteRoutingModule,
     CKEditorModule,
+    NbSelectModule,
+    NbInputModule
   ],
   declarations: [
     ...routedComponents    
